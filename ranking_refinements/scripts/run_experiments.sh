1df0ff010b5cadc061cb48cd79435b48aa52e494
Felix Campbell <felixsal@post.bgu.ac.il>
2024-09-05 16:18:35 +0900

    ditto

--- ranking_refinements/scripts/run_experiments.sh
+++ ranking_refinements/scripts/run_experiments.sh
@@ -2,43 +2,43 @@ PURPLE='\033[0;35m'
 NORMAL='\033[0m'
 
 # echo -e "${PURPLE}Dry run...${NORMAL}"
-echo -e "Running algorithm comparisons...";
+echo -e "${PURPLE}Running algorithm comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/algorithm/*.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
 done;
 
-echo -e "Running constraint bounds comparisons...";
+echo -e "${PURPLE}Running constraint bounds comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/constraints/constraints_bounds/*.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
 done;
 
-echo -e "Running number of constraints comparisons...";
+echo -e "${PURPLE}Running number of constraints comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/constraints/number_of_constraints/*.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
 done;
 
-echo -e "Running k* comparisons...";
+echo -e "${PURPLE}Running k* comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/k/*.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
 done;
 
-echo -e "Running epsilon (maximum average deviation) comparisons...";
+echo -e "${PURPLE}Running epsilon (maximum average deviation) comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/max_dev/*.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
 done;
 
-echo -e "Running predicate type comparisons...";
+echo -e "${PURPLE}Running predicate type comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/predicate/*.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
 done;
 
-echo -e "Running data size comparisons...";
+echo -e "${PURPLE}Running data size comparisons...${NORMAL}";
 for EXPERIMENT in ./experiments_conf/SF/SF_astronauts.py ./experiments_conf/SF/SF_law.py ./experiments_conf/SF/SF_meps.py ./experiments_conf/SF/SF_q5.py; do
     echo -e "${PURPLE}Executing $EXPERIMENT...${NORMAL}";
     python3 $EXPERIMENT;
